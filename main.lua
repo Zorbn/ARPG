41e8afa14c4433e4c52fb6e4b4ce9a819121b476
Zorbn <public.zorb@gmail.com>
2023-04-23 13:00:27 -0700

    Add sword hitbox

--- main.lua
+++ main.lua
@@ -21,6 +21,18 @@ local DOWN = 2
 local LEFT = 3
 local RIGHT = 4
 
+local function checkCollisionRect(r1x1, r1y1, r1x2, r1y2, r2x1, r2y1, r2x2, r2y2)
+    return r1x2 > r2x1 and r1x1 < r2x2 and
+        r1y2 > r2y1 and r1y1 < r2y2
+end
+
+local function rotateAround(x, y, originX, originY, angle)
+    local newX = math.cos(angle) * (x - originX) - math.sin(angle) * (y - originY) + originX
+    local newY = math.sin(angle) * (x - originX) + math.cos(angle) * (y - originY) + originY
+
+    return newX, newY
+end
+
 local player = {
     topSprite = love.graphics.newImage("playerTop.png"),
     bottomSprite = love.graphics.newImage("playerBottom.png"),
@@ -61,6 +73,38 @@ player.sword.swingAngle = player.sword.MAX_SWING_ANGLE
 
 player.arms.width = player.arms.sprite:getWidth()
 player.arms.height = player.arms.sprite:getHeight()
+
+Enemy = {
+    SPEED = 30,
+    MAX_HEALTH = 50,
+    SPRITE = love.graphics.newImage("playerOld.png"),
+}
+
+function Enemy.new(x, y)
+    local enemy = {
+        x = x,
+        y = y,
+        health = Enemy.MAX_HEALTH,
+    }
+
+    function enemy.takeDamage(self, damage)
+        self.health = self.health - damage
+
+        if self.health < 0 then
+            return true
+        end
+
+        return false
+    end
+
+    return enemy
+end
+
+local enemies = {}
+
+table.insert(enemies, Enemy.new(50, 50))
+table.insert(enemies, Enemy.new(25, 25))
+table.insert(enemies, Enemy.new(0, 0))
 
 function love.resize(w, h)
 end
@@ -132,15 +176,40 @@ function love.draw()
     love.graphics.push()
     love.graphics.scale(VIEW_SCALE, VIEW_SCALE)
 
+    local swordAngle = player.sword.angle + player.sword.swingAngle
+
+    local swordHitboxX = player.x + 1.5 * TILE_SIZE
+    local swordHitboxY = player.y + 0.5 * TILE_SIZE
+    swordHitboxX, swordHitboxY = rotateAround(swordHitboxX, swordHitboxY, player.x + TILE_SIZE * 0.5, player.y + TILE_SIZE * 0.5, swordAngle)
+    local swordX1 = swordHitboxX - 0.5 * TILE_SIZE
+    local swordX2 = swordHitboxX + 0.5 * TILE_SIZE
+    local swordY1 = swordHitboxY - 0.5 * TILE_SIZE
+    local swordY2 = swordHitboxY + 0.5 * TILE_SIZE
+
+    for i, enemy in ipairs(enemies) do
+        love.graphics.draw(Enemy.SPRITE, enemy.x, enemy.y)
+
+        if checkCollisionRect(enemy.x, enemy.y, enemy.x + TILE_SIZE,
+            enemy.y + TILE_SIZE, swordX1, swordY1, swordX2, swordY2) then
+
+            if enemy:takeDamage(25) then
+                table.remove(enemies, i)
+            end
+        end
+    end
+
     love.graphics.draw(player.bottomSprite, player.bottomFrames[player.direction], player.x, player.y)
 
-    local swordAngle = player.sword.angle + player.sword.swingAngle
     love.graphics.draw(player.arms.sprite, player.x + player.sword.x, player.y + player.sword.y,
         swordAngle, 1, player.sword.scaleY, -player.arms.width * 0.2, player.arms.height * 0.5)
     love.graphics.draw(player.sword.sprite, player.x + player.sword.x, player.y + player.sword.y,
         swordAngle, 1, player.sword.scaleY, -player.sword.width * 0.6, player.sword.height * 0.5)
 
     love.graphics.draw(player.topSprite, player.topFrames[player.direction], player.x, player.y)
+
+    love.graphics.setColor(1.0, 0.0, 0.0, 1.0)
+    love.graphics.points(swordX1, swordY1, swordX2, swordY2)
+    love.graphics.setColor(1.0, 1.0, 1.0, 1.0)
 
     love.graphics.pop()
 end
